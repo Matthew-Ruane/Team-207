7c1491bac4eef2ed238f2e3ca5c012499ca1f2ba
Matthew-Ruane <49774263+Matthew-Ruane@users.noreply.github.com>
2019-06-03 17:24:52 -0700

    added ring buffer (by kent ross)

---
+++ 2019.java/src/main/java/frc/utility/RingBuffer.java
@@ -0,0 +1,38 @@
+package frc.utility;
+import java.util.Arrays;
+
+/**
+ *
+ * @author Kent Ross
+ */
+
+
+class RingBuffer {
+    final int bufferSize;
+    private int[] valueBuffer;
+    private long[] timeBuffer;
+    private int pos;
+
+    public RingBuffer(int bufferSize, int initialValue) { // buffersize 16, initial value = 0
+        this.bufferSize = bufferSize;
+        valueBuffer = new int[bufferSize];
+        timeBuffer = new long[bufferSize];
+        Arrays.fill(valueBuffer, initialValue);
+        long nowTime = System.nanoTime();
+        Arrays.fill(timeBuffer, nowTime);
+    }
+
+    public void Record(int value) {
+        pos = Math.floorMod(pos + 1, bufferSize);
+        timeBuffer[pos] = System.nanoTime();
+        valueBuffer[pos] = value;
+    }
+
+    // samplesAgo should be a positive value less than bufferSize.
+    public double RateOverSamples(int samplesAgo) {
+        int oldPos = Math.floorMod(pos - samplesAgo, bufferSize);
+        double timeDelta = timeBuffer[pos] - timeBuffer[oldPos];
+        // Time is recorded in nanoseconds, so we multiply by a billion.
+        return (valueBuffer[pos] - valueBuffer[oldPos]) / timeDelta * 1e9;
+    }
+}
